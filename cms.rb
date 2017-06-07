502923530f644078ede724bd03dfe5e4642d75cb
Fernanda Franco <francodias.fernanda@gmail.com>
2017-06-07 19:41:52 -0400

    viewing markdown files

--- cms.rb
+++ cms.rb
@@ -1,12 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
-
-root = File.expand_path("..", __FILE__)
+require "redcarpet"
 
 configure do
   enable :sessions
   set :session_secret, 'super secret'
+end
+
+root = File.expand_path("..", __FILE__)
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
 end
 
 get "/" do
@@ -17,12 +34,12 @@ get "/" do
 end
 
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  filename = params[:filename]
+  file_path = root + "/data/" + filename
   if File.exist?(file_path)
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    load_file_content(file_path)
   else
-    session[:message] = "#{params[:filename]} does not exist."
+    session[:message] = "#{filename} does not exist."
     redirect "/"
   end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -14,9 +14,10 @@ class CMSTest < Minitest::Test
 
   def test_index
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
     assert_includes last_response.body, "history.txt"
   end
@@ -47,5 +48,13 @@ class CMSTest < Minitest::Test
     get last_response["Location"]
 
     assert_includes last_response.body, "notafile.ext does not exist."
+  end
+
+  def test_viewing_md_files
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 end
